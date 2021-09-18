2ef0179396e674e81def7bf47aedfe799865cfb7
KevinNathanaelTaufiek <kevin.taufiek@gmail.com>
2021-09-18 12:47:27 +0700

    Add Setting Module

--- app/src/main/java/com/kevinnt/movapp/SplashScreenActivity.kt
+++ app/src/main/java/com/kevinnt/movapp/SplashScreenActivity.kt
@@ -4,12 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import androidx.appcompat.app.AppCompatDelegate
 import com.kevinnt.movapp.onboarding.OnboardingOneActivity
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
         var handler = Handler()
         handler.postDelayed({
--- app/src/main/java/com/kevinnt/movapp/home/SettingFragment.kt
+++ app/src/main/java/com/kevinnt/movapp/home/SettingFragment.kt
@@ -5,8 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.kevinnt.movapp.R
+import com.kevinnt.movapp.utils.Preferences
+import kotlinx.android.synthetic.main.fragment_setting.*
+
 class SettingFragment : Fragment() {
+
+    private lateinit var preferences : Preferences
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -14,6 +21,21 @@ class SettingFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_setting, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        preferences = Preferences(context!!)
+        var url = preferences.getValue("url");
+
+        Glide.with(context!!)
+                .load(url)
+                .apply(RequestOptions.circleCropTransform())
+                .into(iv_profile)
+
+        tv_name.text = preferences.getValue("nama");
+        tv_email.text = preferences.getValue("email");
+
     }
 
 }
